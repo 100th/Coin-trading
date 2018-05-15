8018606af55d4ddfff760795c3a700501cea37dc
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-05-15 20:50:38 +0900

    set_token 메서드에 대한 단위 테스트
    set_token 메서드에 대한 단위 테스트

--- tests/test_korbit_machine.py
+++ tests/test_korbit_machine.py
@@ -8,5 +8,11 @@ class KorbitMachineTestCase(unittest.TestCase):
         self.korbit_machine = KorbitMachine()
 #        self.korbit_machine.set_token()
 
+    def test_set_token(self):
+        print(inspect.stack()[0][3])
+        expire, access_token, refresh_token = self.korbit_machine.set_token(grant_type="password")
+        assert expire and access_token and refresh_token
+        print("Expire:", expire, "Access_token:", access_token, "Refresh_token:", refresh_token)
+
     def tearDown(self):
         pass
