e0ebf72f2a2c59d062aaa1099fde66d857efd2a3
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-05-15 20:51:57 +0900

    get_token 메서드에 대한 단위 테스트
    get_token 메서드에 대한 단위 테스트

--- tests/test_korbit_machine.py
+++ tests/test_korbit_machine.py
@@ -6,13 +6,20 @@ class KorbitMachineTestCase(unittest.TestCase):
 
     def setUp(self):
         self.korbit_machine = KorbitMachine()
-#        self.korbit_machine.set_token()
+        self.korbit_machine.set_token()
 
     def test_set_token(self):
         print(inspect.stack()[0][3])
         expire, access_token, refresh_token = self.korbit_machine.set_token(grant_type="password")
         assert expire and access_token and refresh_token
         print("Expire:", expire, "Access_token:", access_token, "Refresh_token:", refresh_token)
+
+    def test_get_token(self):
+        print(inspect.stack()[0][3])
+        self.korbit_machine.set_token(grant_type="password")
+        access_token = self.korbit_machine.get_token()
+        assert access_token
+        print(access_token)
 
     def tearDown(self):
         pass
