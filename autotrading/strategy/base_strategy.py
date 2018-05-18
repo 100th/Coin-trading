698da97c23817a5c037815e5b7984cd6c9387884
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-05-18 16:38:22 +0900

    order sell transaction 메서드
    order sell transaction 메서드

--- autotrading/strategy/base_strategy.py
+++ autotrading/strategy/base_strategy.py
@@ -49,6 +49,37 @@ class Strategy(ABC):
             return None
 
     def order_sell_transaction(self, machine=None, db_handler=None, currency_type=None, item=None, order_type="limit"):
-        pass
+        """매도 주문과 함께 DB에 필요한 데이터 업데이트 작업을 위한 메소드
+        Args:
+            machine(obj): 매도 주문하려는 거래소 모듈 객체
+            db_handler(obj): 매도 주문정보를 입력하려는 DB 모듈 객체
+            currency_type(str): 매도 주문하려는 화폐 종류
+            item(dict): 매도 완료후 DB에 저장하려는 데이터
+            order_type(str): 매도 방법
+
+        Returns:
+            OrderId(str):매수 완료 후 주문 id
+        """
+        if currency_type is None or item is None:
+            raise Exception("Need to param")
+        db_handler.set_db_collection("trader","trade_status")
+        result = machine.sell_order(currency_type=currency_type,
+                                        price=str(item["desired_value"]),
+                                        qty=str(round(item["real_buy_amount"],8)),
+                                        order_type=order_type)
+        if result["status"] == "success":
+            db_handler.update_items({"_id":item["_id"]},
+                        {"$set":{"status":"SELL_ORDERED",
+                        "desired_value":int(item["desired_value"]),
+                        "sell_order_id":str(result["orderId"]),
+                        "error":"success"}
+                        })
+            return result["orderId"]
+        else:
+            logger.info(result)
+            logger.info(item)
+            db_handler.update_items({"_id":item["_id"]},{"error": "failed"})
+            return None
+
     def order_cancel_transaction(self, machine=None, db_handler=None, currency_type=None, item=None):
         pass
