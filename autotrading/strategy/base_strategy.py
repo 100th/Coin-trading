25beb90c486583212bac19ae4563418586586110
Jonghyun Lee <36814410+only-hub@users.noreply.github.com>
2018-05-18 16:22:44 +0900

    트레이딩 로직에 대한 추상 클래스
    트레이딩 로직에 대한 추상 클래스

---
+++ autotrading/strategy/base_strategy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+import datetime
+from autotrading.logger import get_logger
+
+logger = get_logger("base_strategy")
+
+class Strategy(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+
+    def update_trade_status(self, db_handler=None, item_id=None, value=None):
+        pass
+    def order_buy_transaction(self, machine=None, db_handler=None, currency_type=None, item=None, order_type="limit"):
+        pass
+    def order_sell_transaction(self, machine=None, db_handler=None, currency_type=None, item=None, order_type="limit"):
+        pass
+    def order_cancel_transaction(self, machine=None, db_handler=None, currency_type=None, item=None):
+        pass
